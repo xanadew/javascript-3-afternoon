fcde1aed22aa783a4fb6692f6db1cfbcd6821221
Greg Marvin <gregmarv@gmail.com>
2018-01-31 16:27:45 -0700

    oh yeah

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,8 +40,14 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  var total='';
+  for(var key in obj){
+
+    total+=obj[key];
+  }
+  return total;
 }
+showValues({names:'bob',otherNames:'rob'});
 
 
 
@@ -54,7 +60,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+  for(var key in obj){
+    if(obj[key]>10){
+      obj[key]=0;
+    }
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +79,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for(var key in obj){
+    obj[key]*=2;
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,6 +98,15 @@ function showValues( obj ) {
 */
 
 //Code Here
+function secrets(obj){
+  var empty='';
+  for(var key in obj){
+    if(key.startsWith('sh')){
+      empty+=obj[key];
+    }
+  }
+  return empty;
+}
 
 
 
@@ -111,7 +138,10 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function removePassword(obj){
+  delete obj.password;
+  return obj;
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -130,7 +160,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(var key in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[key]>100){
+    delete deleteTheBigNumbers[key];
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,7 +177,14 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function startsWithK(obj){
+  for(var key in obj){
+    if(key.startsWith('k')){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -158,5 +199,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function hiddenTreasure(obj){
+  for(var key in obj){
+    if(!obj[key].includes('treasure')){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
